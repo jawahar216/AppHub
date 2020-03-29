adf877fb8651be8d05d2a670bdfc7c032163fd0f
jawahar216 <jawahar216@yahoo.co.uk>
2020-03-29 11:27:37 -0700

    Added linked list code

---
+++ src/com/apphub/algorithm/datastructures/LinkedList.java
@@ -0,0 +1,116 @@
+package com.apphub.algorithm.datastructures;
+
+public class LinkedList<T> {
+
+    Node head = null, tail = null;
+
+    public void addFirst(T data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            temp.setNext(head);
+            head = temp;
+        }
+    }
+
+    public void addAtIndex(T data, int index) {
+        Node temp = new Node(data);
+        Node currentNode = head;
+        Node prevNode = head;
+        int count = 0;
+        if (index == 0) {
+            temp.setNext(head);
+            head = temp;
+        }
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            while (count <= index) {
+                if (count == index) {
+                    prevNode.setNext(temp);
+                    temp.setNext(currentNode);
+                    break;
+                } else {
+                    prevNode = currentNode;
+                    currentNode = currentNode.getNext();
+                    count++;
+                }
+            }
+        }
+    }
+
+    public void addLast(T data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            tail.setNext(temp);
+            tail = tail.getNext();
+        }
+    }
+
+    public void removeFirst() {
+        head = head.getNext();
+    }
+
+    public void removeLast() {
+        Node temp = head;
+        while (temp.getNext() != tail) {
+            temp = temp.getNext();
+        }
+        temp.setNext(null);
+        tail = temp;
+    }
+
+    public void removeAtIndex(int index) {
+        int count = 0;
+        Node currentNode = head;
+        Node prevNode = head;
+        if (index == 0) {
+            head = head.getNext();
+        }
+        while (count <= index) {
+            if (count == index) {
+                prevNode.setNext(currentNode.getNext());
+                currentNode.setNext(null);
+                break;
+            } else {
+                prevNode = currentNode;
+                currentNode = currentNode.getNext();
+                count++;
+            }
+
+        }
+    }
+
+    public int searchAndReturnIndex(T data) {
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+
+            if (data.equals(temp.getData())) {
+                return count;
+            } else {
+                temp = temp.getNext();
+                count++;
+            }
+        }
+        return -1;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+
+    }
+}
+
+
+
---
+++ src/com/apphub/algorithm/datastructures/LinkedListMain.java
@@ -0,0 +1,27 @@
+package com.apphub.algorithm.datastructures;
+
+public class LinkedListMain {
+
+    public static void main(String... args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addFirst(15);
+        list.addLast(12);
+        list.addFirst(13);
+        list.addAtIndex(25, 2);
+        list.addAtIndex(40, 0);
+        list.addLast(42);
+        list.addLast(55);
+        System.out.println("List after adding");
+        list.print();
+        list.removeAtIndex(2);
+        list.removeFirst();
+        list.removeLast();
+        list.removeAtIndex(0);
+        System.out.println("List after removing");
+        list.print();
+        System.out.println("Index of 42 : " + list.searchAndReturnIndex(42));
+        System.out.println("Index of 13 : " + list.searchAndReturnIndex(13));
+
+    }
+}
